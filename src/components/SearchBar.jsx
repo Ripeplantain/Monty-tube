36e95c3c94e9e8620b71811d05b835ba661a1ea7
ripeplantain <irvingmanny@gmail.com>
2023-09-17 22:45:18 +0000

    search is responsive

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,51 +3,58 @@ import { useNavigate } from 'react-router-dom';
 import { Paper, IconButton } from '@mui/material';
 import { Search } from '@mui/icons-material';
 
-
-
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
 
-    const [ searchTerm, setSearchTerm ] = useState('');
-    const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        if(searchTerm){
-            navigate(`/search/${searchTerm}`)
-            setSearchTerm('');
-        }
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+      setSearchTerm('');
     }
-
+  };
 
   return (
     <Paper
-        component="form"
-        onSubmit={handleSubmit}
-        sx={{
-            borderRadius:20,
-            border:'1px solid #e3e3e3',
-            pl:2,
-            boxShadow:'none',
-            mr: {sm:5},
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{
+        borderRadius: 20,
+        border: '1px solid #e3e3e3',
+        pl: 2,
+        boxShadow: 'none',
+        mr: { sm: 5 },
+        // width: '100%',
+        display: 'flex', 
+        flexDirection: 'row', 
+        alignItems: 'center', 
+        boxSizing: 'border-box', 
+        justifyContent: 'space-between',
+        padding: { xs: '5px', sm: '10px' }, 
+      }}
+    >
+      <input
+        className="search-bar"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
         }}
-        >
+        style={{
+          flex: 1, 
+          border: 'none',
+          outline: 'none',
+          fontSize: '14px',
+        }}
+      />
 
-        <input 
-            className='search-bar'
-            placeholder='search...'
-            value={searchTerm}
-            onChange={(e)=>{setSearchTerm(e.target.value)}}
-        />
-
-        <IconButton type='submit' sx={{
-            p:'10px', color:'red'
-        }}>
-            <Search />
-        </IconButton>
-
+      <IconButton type="submit" sx={{ p: '7px', color: 'red', flexShrink: 0 }}>
+        <Search />
+      </IconButton>
     </Paper>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
